0980d1edd4d06ffb14e99138474b61d44f457809
yoonseo choi <choiyoonseo02@gmail.com>
2023-11-11 19:52:20 +0900

    feat(OutputView): 초반 안내 멘트 출력

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,14 @@
+package christmas.view;
+
+public class OutputView {
+    public OutputView() {
+    }
+
+    public void startMessage() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public void showEventBenefitsMessage() {
+        System.out.println("12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"); //날짜는 변경할 수 있게끔 수정 필요
+    }
+}
