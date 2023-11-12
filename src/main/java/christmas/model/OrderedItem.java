70064e422411d5b390ea900142bbcc4961c49771
yoonseo choi <choiyoonseo02@gmail.com>
2023-11-12 17:52:34 +0900

    feat: 메뉴의 개수 검사

--- src/main/java/christmas/model/OrderedItem.java
+++ src/main/java/christmas/model/OrderedItem.java
@@ -8,17 +8,18 @@ public class OrderedItem {
     private final int quantity;
 
     public OrderedItem(String menu, int quantity) {
-        validator(menu);
+        validator(menu, quantity);
         this.menu = menu;
         this.quantity = quantity;
     }
 
-    private void validator(String menu) {
-        if (!menuValidator(menu)) {
+    private void validator(String menu, int quantity) {
+        if (!menuValidator(menu) || !quantityValidator(quantity)) {
             throw new IllegalArgumentException();
         }
     }
 
+    // 메뉴판에 있는 메뉴인지 검사
     private boolean menuValidator(String menu) {
         for (Menu existedMenu : Menu.values()) {
             if (Objects.equals(menu, existedMenu.getName())) {
@@ -26,5 +27,10 @@ public class OrderedItem {
             }
         }
         return false;
+    }
+
+    //개수 검사
+    private boolean quantityValidator(int quantity) {
+        return 1 <= quantity && quantity <= 20;
     }
 }
