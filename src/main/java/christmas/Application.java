c8d0493ea841a5ba38e37fee9ac36ed301c2ea8e
yoonseo choi <choiyoonseo02@gmail.com>
2023-11-14 00:41:32 +0900

    feat: Application 작동 로직

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,12 @@
 package christmas;
 
+import christmas.domain.Controller;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Controller controller = new Controller(new OutputView(), new InputView());
+        controller.eventPlannerLogic();
     }
 }
