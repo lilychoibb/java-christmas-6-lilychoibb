ed3d7a0a96bf1779d354888c421e93b1db8969c0
yoonseo choi <choiyoonseo02@gmail.com>
2023-11-13 18:30:04 +0900

    feat: 평일 및 주말 할인

--- src/main/java/christmas/model/Discount.java
+++ src/main/java/christmas/model/Discount.java
@@ -1,5 +1,9 @@
 package christmas.model;
 
+import christmas.domain.Menu;
+import java.util.List;
+import java.util.Objects;
+
 public class Discount {
     public static final int WEEKDAY = 2023;
     public static final int WEEKEND = 2023;
@@ -11,12 +15,32 @@ public class Discount {
     }
 
     private int calculateChristmasDDayDiscount(ExpectedVisitDate expectedVisitDate) {
-        if(expectedVisitDate.getExpectedVisitDate() <= 25){
+        if (expectedVisitDate.getExpectedVisitDate() <= 25) {
             for (int i = 1; i < expectedVisitDate.getExpectedVisitDate(); i++) {
                 christmasDDay += 100;
             }
         }
 
         return christmasDDay;
+    }
+
+    private int calculateWeekDayDiscount(List<OrderedItem> orderedItems) {
+        for (OrderedItem item:orderedItems) {
+            Menu menu = Menu.valueOf(item.getMenu());
+            if (Objects.equals(menu.getType(), "dessert")) {
+                return menu.getPrice() - WEEKDAY;
+            }
+        }
+        return 0;
+    }
+
+    private int calculateWeekendDiscount(List<OrderedItem> orderedItems){
+        for (OrderedItem item:orderedItems) {
+            Menu menu = Menu.valueOf(item.getMenu());
+            if (Objects.equals(menu.getType(), "main")) {
+                return menu.getPrice() - WEEKEND;
+            }
+        }
+        return 0;
     }
 }
